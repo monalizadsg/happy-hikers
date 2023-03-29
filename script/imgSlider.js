f59a8c618d8e34cd8be396f8cf885d418877e4c7
Monaliza Dasig <monalizadasig@gmail.com>
2023-03-28 18:24:53 -0700

    add heading img slider

---
+++ script/imgSlider.js
@@ -0,0 +1,28 @@
+// image slider
+const slides = document.querySelectorAll(".img-slide");
+const contents = document.querySelectorAll(".content");
+let currentSlide = 0;
+
+// console.log(slides);
+// console.log(contents);
+
+function slideImages() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  if (currentSlide === slides.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  slides[currentSlide].classList.add("active");
+  contents[currentSlide].classList.add("active");
+}
+const interval = 6000; // duration of the slide
+setInterval(slideImages, interval); // automatically change the slide
