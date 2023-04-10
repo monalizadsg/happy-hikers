eadd895609a902a73179b114f0db767412d15f64
Monaliza Dasig <monalizadasig@gmail.com>
2023-04-10 10:43:26 -0700

    Add contact form css validation

--- script/sendMessage.js
+++ script/sendMessage.js
@@ -1,69 +1,93 @@
 function sendMessage() {
-    let name = document.getElementById("input_name").value;
-    let phone = document.getElementById("input_tel").value;
-    let email = document.getElementById("input_email").value;
-    let message = document.getElementById("input_message").value;
-      Email.send({
-        Host: "smtp.elasticemail.com",
-        Username: "milan.destura620@gmail.com",
-        Password: "3D7250B8538677416DE47D366AA6EA19F4F6",
-        To: 'milan.destura007@gmail.com',//change this to desired recipient(Happy Hikers Admin email)
-        From: "milan.destura620@gmail.com",
-        Subject: "Happy Hikers : Mail from contact form",
-        Body: "Name:" +name+ "<br/> Email:"+ email + "<br/> Phone:" +phone+ "<br/> Message:" + message
-      }).then(
-        message => alert(message)
-      );
-      alert("Message Sent.");
-       }
-
-    function validate() {
-      let name = document.getElementById("input_name").value;
-      let phone = document.getElementById("input_tel").value;
-      let email = document.getElementById("input_email").value;
-      let message = document.getElementById("input_message").value;
-      let isValid=true;
-  
-    if (!name) {
-      alert('Name is required');
-      return false;
-  }
-  if (!phone) {
-      alert('Phone is required');
-      return false;
-  } else if (!validatePhoneNumber(phone)) {
-    alert('Phone number is invalid');
-    return false;
+  let name = document.getElementById("input_name").value;
+  let phone = document.getElementById("input_tel").value;
+  let email = document.getElementById("input_email").value;
+  let message = document.getElementById("input_message").value;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "milan.destura620@gmail.com",
+    Password: "3D7250B8538677416DE47D366AA6EA19F4F6",
+    To: "milan.destura007@gmail.com", //change this to desired recipient(Happy Hikers Admin email)
+    From: "milan.destura620@gmail.com",
+    Subject: "Happy Hikers : Mail from contact form",
+    Body:
+      "Name:" +
+      name +
+      "<br/> Email:" +
+      email +
+      "<br/> Phone:" +
+      phone +
+      "<br/> Message:" +
+      message,
+  }).then((message) => alert(message));
+  alert("Message Sent.");
 }
-  if (!email) {
-      alert('Email is required');
-      return false;
-  } else if (!isValidEmail(email)) {
-      alert('Email is invalid');
-      return false;
+
+function validate() {
+  let name = document.getElementById("input_name").value;
+  let phone = document.getElementById("input_tel").value;
+  let email = document.getElementById("input_email").value;
+  let message = document.getElementById("input_message").value;
+
+  // reset fields on submit
+  clearErrorMessage();
+
+  if (!name) {
+    alert("Name is required");
+    document.getElementById("input_name").classList.add("error");
+    return false;
   }
 
+  if (!email) {
+    alert("Email is required");
+    document.getElementById("input_email").classList.add("error");
+    return false;
+  } else if (!isValidEmail(email)) {
+    alert("Email is invalid");
+    document.getElementById("input_email").classList.add("error");
+    return false;
+  }
+
+  if (!phone) {
+    alert("Phone is required");
+    document.getElementById("input_tel").classList.add("error");
+    return false;
+  } else if (!validatePhoneNumber(phone)) {
+    alert("Phone number is invalid");
+    document.getElementById("input_tel").classList.add("error");
+    return false;
+  }
 
   if (!message) {
-      alert('Message is required');
-      return false;
+    alert("Message is required");
+    document.getElementById("input_message").classList.add("error");
+    return false;
   }
 
   return true;
-    }
-    
+}
+
 function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
 
 function validatePhoneNumber(phoneNumber) {
-  const phoneRegex = /^\d{3}\d{3}\d{4}$/; // regular expression for format XXX-XXX-XXXX
+  const phoneRegex = /^\+?\d{3}\d{3}\d{4}$/; // regular expression for format XXX-XXX-XXXX
   return phoneRegex.test(phoneNumber);
 }
 
-function validate1(){
-if(validate()){
-  sendMessage();
+function clearErrorMessage() {
+  document.getElementById("input_name").classList.remove("error");
+  document.getElementById("input_email").classList.remove("error");
+  document.getElementById("input_tel").classList.remove("error");
+  document.getElementById("input_message").classList.remove("error");
 }
+
+function validateForm(e) {
+  if (e) e.preventDefault(); // prevent form submission to avoid page refresh every click on submit
+  const isValid = validate();
+  if (isValid) {
+    sendMessage();
+  }
 }
